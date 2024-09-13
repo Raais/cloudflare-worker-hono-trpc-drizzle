0b39ba4dedddf658092a29003fe21e2921b0fe79
Raais <raais@pm.me>
2024-09-13 17:45:33 +0500

    rate limit

--- src/routes/maptiles.ts
+++ src/routes/maptiles.ts
@@ -1,6 +1,6 @@
 import { Hono } from "hono";
 import { Buffer } from "node:buffer";
-import { isPointInPolygon } from 'geolib';
+import { isPointInPolygon } from "geolib";
 
 const MAX_HITS = 500; // 200,000
 const PREFIX = "_tiles";
@@ -12,6 +12,7 @@ const tiles = new Hono<{
     TILES: KVNamespace;
     MAP_API_KEYNAME: string;
     MAP_API_KEY: string;
+    CFW_RATE_LIMITER: any;
   };
 }>();
 
@@ -21,33 +22,39 @@ tiles.get("/", (c) => c.text("Dev map tiles proxy API.", 200));
 // https://cfw.raa.is/map/tiles/static/{z}/{x}/{y}
 
 tiles.get("/static/:z/:x/:y", async (c) => {
-
   const KEYNAME = c.env.MAP_API_KEYNAME;
   const KEY = c.env.MAP_API_KEY;
 
   const z = c.req.param("z");
   const x = c.req.param("x");
   const y = c.req.param("y");
-  
+
+  const carto_url = `https://cartodb-basemaps-b.global.ssl.fastly.net/dark_all/${z}/${x}/${y}.png`;
+
+  const { success } = await c.env.CFW_RATE_LIMITER.limit({ key: KEYNAME });
+  if (!success) {
+    // fallback
+    const carto = await fetch(carto_url);
+    return new Response(carto.body, carto);
+  }
+
   function tile2long(x: number, z: number) {
-    return (x/Math.pow(2,z)*360-180);
+    return (x / Math.pow(2, z)) * 360 - 180;
   }
   function tile2lat(y: number, z: number) {
-    var n=Math.PI-2*Math.PI*y/Math.pow(2,z);
-    return (180/Math.PI*Math.atan(0.5*(Math.exp(n)-Math.exp(-n))));
+    var n = Math.PI - (2 * Math.PI * y) / Math.pow(2, z);
+    return (180 / Math.PI) * Math.atan(0.5 * (Math.exp(n) - Math.exp(-n)));
   }
-  
+
   const lat = tile2lat(parseInt(y), parseInt(z));
   const long = tile2long(parseInt(x), parseInt(z));
-  
+
   const ALLOWED_BOUNDS = [
     { latitude: 5.27402109359528, longitude: 71.66117980975153 },
     { latitude: 5.3420912644821215, longitude: 74.99286272485952 },
     { latitude: 3.3789574137604745, longitude: 75.01565127022587 },
     { latitude: 3.2333535062166447, longitude: 71.61560030165396 },
-  ]
-
-  const carto_url = `https://cartodb-basemaps-b.global.ssl.fastly.net/dark_all/${z}/${x}/${y}.png`;
+  ];
 
   if (!isPointInPolygon({ latitude: lat, longitude: long }, ALLOWED_BOUNDS)) {
     // fallback
@@ -61,7 +68,7 @@ tiles.get("/static/:z/:x/:y", async (c) => {
 
   if (cached) {
     const res = JSON.parse(cached);
-    
+
     let headers = res.headers;
     headers["cfw-cached"] = "true";
 
@@ -81,10 +88,10 @@ tiles.get("/static/:z/:x/:y", async (c) => {
   if (hits) {
     const current = parseInt(hits);
     if (current >= MAX_HITS) {
-        //   return c.text("Too many requests.", 429);
-        // fallback
-        const carto = await fetch(carto_url);
-        return new Response(carto.body, carto);
+      //   return c.text("Too many requests.", 429);
+      // fallback
+      const carto = await fetch(carto_url);
+      return new Response(carto.body, carto);
     } else {
       const newHits = current + 1;
       await c.env.KV.put(prefix(KEYNAME), newHits.toString());
