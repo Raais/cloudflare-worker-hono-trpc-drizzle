20fa32d0627480303b521dc05caec5eb5e3efe0b
Raais <raais@pm.me>
2024-09-26 23:17:04 +0500

    arcgis

--- src/routes/maptiles.ts
+++ src/routes/maptiles.ts
@@ -6,6 +6,39 @@ const MAX_HITS = 500; // 200,000
 const PREFIX = "_tiles";
 const prefix = (key: string) => `${PREFIX}:${key}`;
 
+const ALLOWED_BOUNDS_GREATER_MALE = [
+  { latitude: 5.27402109359528, longitude: 71.66117980975153 },
+  { latitude: 5.3420912644821215, longitude: 74.99286272485952 },
+  { latitude: 3.3789574137604745, longitude: 75.01565127022587 },
+  { latitude: 3.2333535062166447, longitude: 71.61560030165396 },
+];
+
+const BOUNDS_HULHUMALE = [
+  { latitude: 4.196813177657901, longitude: 73.54277870432617 },
+  { latitude: 4.215803368185959, longitude: 73.54862589056742 },
+  { latitude: 4.239804064084623, longitude: 73.55562518327312 },
+  { latitude: 4.243724427459853, longitude: 73.55078317993267 },
+  { latitude: 4.241429611682747, longitude: 73.53951718062171 },
+  { latitude: 4.235644661082858, longitude: 73.53707220863734 },
+  { latitude: 4.220154193371471, longitude: 73.5306002295433 },
+  { latitude: 4.218820738055996, longitude: 73.5341273522975 },
+  { latitude: 4.207605889267228, longitude: 73.53344980817062 },
+  { latitude: 4.198305009527061, longitude: 73.5381356627768 },
+  { latitude: 4.196813177657901, longitude: 73.54277870432617 },
+];
+
+const BOUNDS_GULHIFALHU_THILAFUSHI = [
+  { latitude: 4.189499814904821, longitude: 73.46923237700565 },
+  { latitude: 4.1868001172472304, longitude: 73.45368748137071 },
+  { latitude: 4.193380496508894, longitude: 73.41342918256527 },
+  { latitude: 4.182113107828101, longitude: 73.4030222569042 },
+  { latitude: 4.177158563626861, longitude: 73.44262809799508 },
+  { latitude: 4.169599586624358, longitude: 73.479982406999 },
+  { latitude: 4.177158647544331, longitude: 73.48021442203509 },
+  { latitude: 4.187185860839648, longitude: 73.47387267772257 },
+  { latitude: 4.189499814904821, longitude: 73.46923237700565 },
+];
+
 const tiles = new Hono<{
   Bindings: {
     KV: KVNamespace;
@@ -29,14 +62,13 @@ tiles.get("/static/:z/:x/:y", async (c) => {
   const x = c.req.param("x");
   const y = c.req.param("y");
 
-  const carto_url = `https://cartodb-basemaps-b.global.ssl.fastly.net/dark_all/${z}/${x}/${y}.png`;
+  const ARCGIS_URL = `https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/${z}/${y}/${x}`;
+  const CARTO_URL = `https://cartodb-basemaps-b.global.ssl.fastly.net/dark_all/${z}/${x}/${y}.png`;
 
-  const { success } = await c.env.CFW_RATE_LIMITER.limit({ key: KEYNAME });
-  if (!success) {
-    // fallback
-    const carto = await fetch(carto_url);
-    return new Response(carto.body, carto);
-  }
+  // const { success } = await c.env.CFW_RATE_LIMITER.limit({ key: KEYNAME });
+  // if (!success) {
+  //   return c.text(`Error`, 400);
+  // }
 
   function tile2long(x: number, z: number) {
     return (x / Math.pow(2, z)) * 360 - 180;
@@ -49,29 +81,28 @@ tiles.get("/static/:z/:x/:y", async (c) => {
   const lat = tile2lat(parseInt(y), parseInt(z));
   const long = tile2long(parseInt(x), parseInt(z));
 
-  const ALLOWED_BOUNDS = [
-    { latitude: 5.27402109359528, longitude: 71.66117980975153 },
-    { latitude: 5.3420912644821215, longitude: 74.99286272485952 },
-    { latitude: 3.3789574137604745, longitude: 75.01565127022587 },
-    { latitude: 3.2333535062166447, longitude: 71.61560030165396 },
-  ];
+  if (!isPointInPolygon({ latitude: lat, longitude: long }, ALLOWED_BOUNDS_GREATER_MALE)) {
+    return c.text(`Error`, 400);
+  }
 
-  if (!isPointInPolygon({ latitude: lat, longitude: long }, ALLOWED_BOUNDS)) {
-    // fallback
-    const carto = await fetch(carto_url);
-    return new Response(carto.body, carto);
+  if (
+    isPointInPolygon({ latitude: lat, longitude: long }, BOUNDS_HULHUMALE) ||
+    isPointInPolygon(
+      { latitude: lat, longitude: long },
+      BOUNDS_GULHIFALHU_THILAFUSHI
+    )
+  ) {
+    const arcgis = await fetch(ARCGIS_URL);
+    return new Response(arcgis.body, arcgis);
   }
 
   const cacheKey = `cfwtiles:${z}:${x}:${y}`;
-
   const cached = await c.env.TILES.get(cacheKey);
 
   if (cached) {
     const res = JSON.parse(cached);
-
     let headers = res.headers;
     headers["cfw-cached"] = "true";
-
     const options = {
       headers: headers,
       status: 200,
@@ -80,7 +111,6 @@ tiles.get("/static/:z/:x/:y", async (c) => {
       redirected: false,
       webSocket: null,
     };
-
     return new Response(Buffer.from(res.bytes, "base64"), options);
   }
 
@@ -88,10 +118,7 @@ tiles.get("/static/:z/:x/:y", async (c) => {
   if (hits) {
     const current = parseInt(hits);
     if (current >= MAX_HITS) {
-      //   return c.text("Too many requests.", 429);
-      // fallback
-      const carto = await fetch(carto_url);
-      return new Response(carto.body, carto);
+      return c.text(`Error`, 400);
     } else {
       const newHits = current + 1;
       await c.env.KV.put(prefix(KEYNAME), newHits.toString());
@@ -119,7 +146,7 @@ tiles.get("/static/:z/:x/:y", async (c) => {
     };
 
     await c.env.TILES.put(cacheKey, JSON.stringify(serialize), {
-      expirationTtl: 24 * 60 * 60 * 5, // 5 days
+      expirationTtl: 24 * 60 * 60 * 1, // 1 days
     });
 
     const options = {
